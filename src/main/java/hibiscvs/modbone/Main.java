b1294438d0bdf8d7f84cbbfb5afaa64a86c98bc4
hibi <69405847+hibiii@users.noreply.github.com>
2026-05-21 15:52:08 -0300

    more main functionality

--- src/main/java/hibiscvs/modbone/Main.java
+++ src/main/java/hibiscvs/modbone/Main.java
@@ -2,6 +2,7 @@ package hibiscvs.modbone;
 
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -15,21 +16,16 @@ public class Main {
     public static final String USER_AGENT = "hibiii/modbone/1.0a1 (hibiscus.pet)";
     public static final String VERSION_INFORMATION = "modbone 1.0a1, made by hibi, licensed under MIT";
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws Exception {
         parseArgs(args);
-        List<Mod> mods = null;
-        try {mods = MonitorEngine.loadModList();}
-        catch(FileNotFoundException e) {
-            System.err.println("Could not load the mod definitions list: %s".formatted(e.getMessage()));
-            System.exit(5);
+        for (Task task : tasks) {
+            task.execute();
         }
-        try {
-            Database.getDatabase();
-        } catch (SQLException e) {
-            System.err.println("Could not load the database: %s".formatted(e.getMessage()));
-            System.exit(5);
+        if (tasks.isEmpty()) {
+            System.err.println("No tasks requested.");
+            System.err.println(USAGE_TEXT);
+            System.exit(0);
         }
-        MonitorEngine.performForAllMods(mods);
     }
 
     public static String getModDefPath() {
@@ -52,7 +48,8 @@ public class Main {
                     System.out.println(USAGE_TEXT);
                     System.exit(0); })
                 | argumentMatches(arg, "--mods-file:", (text) -> modDefPath = text)
-                | argumentMatches(arg, "--database-file:", (text) -> dbPath = text);
+                | argumentMatches(arg, "--database-file:", (text) -> dbPath = text)
+                | argumentMatches(arg, "monitor", (_) -> addTask(Task.MONITOR));
             if (hasMatched == false) {
                 System.err.println("Unknown argument \"%s\".".formatted(arg));
                 System.exit(1);
@@ -70,7 +67,11 @@ public class Main {
 
     private static final String USAGE_TEXT =
     """
-    The following parameters are available to use from the command line:
+    The following tasks are available and at least one must be present:
+    monitor
+        Get current download metrics for the configured mods.
+    
+    The following options are available to use from the command line:
     --help
         Shows this help text and exits.
     --version
@@ -79,6 +80,44 @@ public class Main {
         Specify a different path to load the mod definition list from. Defaults to `./mods.json`.
     --database-file:<SQLite database>
         Specify a different path to load the database in which to store download
-        records. Defaults to `./records.db`.
+        records. DefaultsR)  to `./records.db`.
     """;
+
+    private static List<Mod> mods = null;
+    private static List<Task> tasks = new ArrayList<>(1);
+
+    private static void addTask(Task task) {
+        if (tasks.contains(task)) {
+            return;
+        }
+        tasks.addLast(task);
+    }
+
+    private static enum Task {
+        NONE,
+        MONITOR { @Override public void execute() throws SQLException {
+            if (Main.mods == null) {
+                try {MonitorEngine.loadModList();}
+                catch(FileNotFoundException e) {
+                    System.err.println("Could not load the mod definitions list: %s".formatted(e.getMessage()));
+                    System.exit(1);
+                }
+            }
+            Database db = null;
+            try {
+                db = Database.getDatabase();
+            } catch (SQLException e) {
+                System.err.println("Could not load the database: %s".formatted(e.getMessage()));
+                System.exit(5);
+            }
+            if (db.hasTodaysRecords()) {
+                System.err.println("INFO: There's already records for today, ignoring");
+                return;
+            }
+            MonitorEngine.performForAllMods(mods);
+        }},
+        ;
+
+        public void execute() throws Exception {}
+    }
 }
